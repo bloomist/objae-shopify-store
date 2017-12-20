83c38d49596fb8bbbdb272fc71b7521f19b1cbbe
Tiff Ting <tiff.ting@gmail.com>
2017-12-20 15:51:41 +0800

    Revert (buggy) image click variant selection
    This reverts commit 8aa3e2063ce59979bf95ca5cc3347f57d0e75abb.

--- assets/theme.js
+++ assets/theme.js
@@ -2963,27 +2963,3 @@ theme.init = function() {
 };
 
 $(theme.init);
-
-
-/*
-Select product variants by clicking their images: https://help.shopify.com/themes/customization/products/variants/select-variants-by-clicking-images
-*/
-$(document).ready(function() {
-  thumbnails = $('img[src*="/products/"]').not(':first');
-  if (thumbnails.length) {
-    thumbnails.bind('click', function() {
-      var arrImage = $(this).attr('src').split('?')[0].split('.');
-      var strExtention = arrImage.pop();
-      var strRemaining = arrImage.pop().replace(/_[a-zA-Z0-9@]+$/,'');
-      var strNewImage = arrImage.join('.')+"."+strRemaining+"."+strExtention;
-      if (typeof variantImages[strNewImage] !== 'undefined') {
-          productOptions.forEach(function (value, i) {
-           optionValue = variantImages[strNewImage]['option-'+i];
-           if (optionValue !== null && $('.single-option-selector:eq('+i+') option').filter(function() { return $(this).text() === optionValue }).length) {
-             $('.single-option-selector:eq('+i+')').val(optionValue).trigger('change');
-           }
-        });
-      }
-    });
-  }
-});
